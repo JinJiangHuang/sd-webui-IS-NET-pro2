df470e7dc0302c3c77419186fc4853b6e6d208aa
JinJiangHuang <1062951657@qq.com>
2023-04-21 13:35:15 +0800

    Update video2frame.py
    关键帧提取

--- isnet_pro/video2frame.py
+++ isnet_pro/video2frame.py
@@ -1,7 +1,12 @@
 import cv2
 import os
-from tqdm import tqdm
+import operator
+import matplotlib.pyplot as plt
+import sys
 import numpy as np
+from scipy.signal import argrelextrema
+from tqdm import tqdm
+
 def ui_frame2video(image_folder,ouput_dir,fps,mode):
     print("\n IS-NET_pro:frame2video generating...")
     if mode =='.mp4':
@@ -10,8 +15,90 @@ def ui_frame2video(image_folder,ouput_dir,fps,mode):
         return frame2video_alpga(image_folder,ouput_dir,fps)
 
 
+def smooth(x, window_len=13, window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+    output:
+        the smoothed signal
+        
+    example:
+    import numpy as np    
+    t = np.linspace(-2,2,0.1)
+    x = np.sin(t)+np.random.randn(len(t))*0.1
+    y = smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string   
+    """
+    print(len(x), window_len)
+    # if x.ndim != 1:
+    #     raise ValueError, "smooth only accepts 1 dimension arrays."
+    #
+    # if x.size < window_len:
+    #     raise ValueError, "Input vector needs to be bigger than window size."
+    #
+    # if window_len < 3:
+    #     return x
+    #
+    # if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+    #     raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+ 
+    s = np.r_[2 * x[0] - x[window_len:1:-1],
+              x, 2 * x[-1] - x[-1:-window_len:-1]]
+    #print(len(s))
+ 
+    if window == 'flat':  # moving average
+        w = np.ones(window_len, 'd')
+    else:
+        w = getattr(np, window)(window_len)
+    y = np.convolve(w / w.sum(), s, mode='same')
+    return y[window_len - 1:-window_len + 1]
+ 
 
-def video2frame(video_path,output_folder,aim_fps_checkbox,aim_fps,time_range_checkbox,start_time,end_time):
+class Frame:
+    """class to hold information about each frame
+    
+    """
+    def __init__(self, id, diff):
+        self.id = id
+        self.diff = diff
+ 
+    def __lt__(self, other):
+        if self.id == other.id:
+            return self.id < other.id
+        return self.id < other.id
+ 
+    def __gt__(self, other):
+        return other.__lt__(self)
+ 
+    def __eq__(self, other):
+        return self.id == other.id and self.id == other.id
+ 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+ 
+ 
+def rel_change(a, b):
+   x = (b - a) / max(a, b)
+   print(x)
+   return x
+
+
+def video2frame(video_path,output_folder,aim_fps_checkbox,aim_fps,time_range_checkbox,start_time,end_time,keyframe_checkbox,len_window):
     print("\n IS-NET_pro:video2frame generating...")
 
     # 读取视频文件
@@ -61,6 +148,51 @@ def video2frame(video_path,output_folder,aim_fps_checkbox,aim_fps,time_range_che
                 # 保存帧到输出文件
                 cv2.imwrite(output_file, frame)
                 frame_count += 1
+    elif(keyframe_checkbox):
+        # len_window = int(5)
+        curr_frame = None
+        prev_frame = None 
+        frame_diffs = []
+        frames = []
+        success, frame = cap.read()
+        i = 0 
+        while(success):
+                luv = cv2.cvtColor(frame, cv2.COLOR_BGR2LUV)
+                curr_frame = luv
+                if curr_frame is not None and prev_frame is not None:
+                        #logic here
+                        diff = cv2.absdiff(curr_frame, prev_frame)
+                        diff_sum = np.sum(diff)
+                        diff_sum_mean = diff_sum / (diff.shape[0] * diff.shape[1])
+                        frame_diffs.append(diff_sum_mean)
+                        frame = Frame(i, diff_sum_mean)
+                        frames.append(frame)
+                prev_frame = curr_frame
+                i = i + 1
+                success, frame = cap.read()   
+        cap.release()
+    
+        # compute keyframe
+        keyframe_id_set = set()
+        diff_array = np.array(frame_diffs)
+        sm_diff_array = smooth(diff_array, len_window)
+        frame_indexes = np.asarray(argrelextrema(sm_diff_array, np.greater))[0]
+        for i in frame_indexes:
+            keyframe_id_set.add(frames[i - 1].id)
+        # save all keyframes as image
+        cap = cv2.VideoCapture(str(video_path))
+        curr_frame = None
+        keyframes = []
+        success, frame = cap.read()
+        idx = 0
+        while(success):
+                if idx in keyframe_id_set:
+                        name = "keyframe_" + str(idx).zfill(4) + ".png"
+                        cv2.imwrite(output_folder + name, frame)
+                        keyframe_id_set.remove(idx)
+                idx = idx + 1
+                success, frame = cap.read()
+        cap.release()
     else:
         # 逐帧读取视频并保存到输出文件夹
         frame_count = 1
